2868e6494205440e7d88492afa9598fca5a24395
lrflorescor <lrflorescor@gmail.com>
2022-10-17 09:36:30 -0600

    los 2 metodos para poder agregar y sacar datos de la lista de datos

--- Lab5_LesterFlores_1214119url/Pokedex.cpp
+++ Lab5_LesterFlores_1214119url/Pokedex.cpp
@@ -1,1 +1,24 @@
 #include "Pokedex.h"
+
+#include <vector>
+
+void Pokedex::AgregarLista(int gNumber, int nNumber, string nombre)
+{
+	Node* nuevo = new Node();
+	nuevo->nGeneracion = gNumber;
+	nuevo->nNational = nNumber;
+	nuevo->nombre = nombre;
+	nuevo->next = header;
+	header = nuevo;
+}
+
+void Pokedex::SacarLista(int& gNumber, int& nNumber, string& pNombre)
+{
+	Node* actual = new Node();
+	actual = header;
+	gNumber = actual->nGeneracion;
+	nNumber = actual->nNational;
+	pNombre = actual->nombre;
+	header = actual->next;
+	delete actual;
+}
